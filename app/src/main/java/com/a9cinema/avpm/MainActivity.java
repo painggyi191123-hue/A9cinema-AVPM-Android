0a369142da839ba24f81546f52fc36344c034277
A9cinema <painggyi191123@gmail.com>
2026-09-07 13:08:57 +0630

    Update MainActivity.java

--- app/src/main/java/com/a9cinema/avpm/MainActivity.java
+++ app/src/main/java/com/a9cinema/avpm/MainActivity.java
@@ -4,25 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
 
         WebSettings settings = webView.getSettings();
+
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
 
-        webView.setWebViewClient(new android.webkit.WebViewClient());
+        // Performance
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setDatabaseEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+
+        // Better scrolling
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
+
+        webView.setWebViewClient(new WebViewClient());
 
         webView.loadUrl("file:///android_asset/index.html");
 
         setContentView(webView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.stopLoading();
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
